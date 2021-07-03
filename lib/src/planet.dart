1edbe7a04aa56e9cdc5f0b7f7789a1c01efdceb6
kylee-hal <halekyl@oregonstate.edu>
2021-07-03 17:11:56 -0400

    Update of working version before implementing JSON

---
+++ lib/src/planet.dart
@@ -0,0 +1,8 @@
+class Planet {
+  final String name;
+  final String description;
+
+  Planet({required this.name, required this.description});
+
+  Planet.nullPlanet() : name = 'Null', description = 'Null Planet';
+}
